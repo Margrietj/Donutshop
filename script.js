ce1a1c31b1325d926f302d9796572e0b47c5b151
Margrietj <margrietjellema32@gmail.com>
2022-05-13 11:40:01 +0200

    sdfsdf
    sdfs

---
+++ script.js
@@ -0,0 +1,56 @@
+function DeegNormaal() {
+    document.getElementById("img-dough").src="/links/Plaindough.png";
+}
+ 
+function DeegKaneel() {
+    document.getElementById("img-dough").src="/links/Cinnamondough.png";
+}
+
+function DeegChocolade() {
+    document.getElementById("img-dough").src="/links/Chocolatdough.png";
+}
+
+function DeegRedvelvet() {
+    document.getElementById("img-dough").src="/links/Redvelvetdough.png";
+}
+
+
+
+function GlazuurSuiker() {
+    document.getElementById("img-glaze").src="/links/Glaze_Sugar.png";
+}
+ 
+function GlazuurAardbei() {
+    document.getElementById("img-glaze").src="/links/Glaze_Strawberry.png";
+}
+
+function GlazuurBosbes() {
+    document.getElementById("img-glaze").src="/links/Glaze_Bosbes.png";
+}
+
+function GlazuurBanaan() {
+    document.getElementById("img-glaze").src="/links/Glaze_Banana.png";
+}
+
+function GlazuurChocolade() {
+    document.getElementById("img-glaze").src="/links/Glaze_Chocolat.png";
+}
+
+
+function ToppingMultiSprinkels() {
+    document.getElementById("img-topping").src="/links/Topping_SprinklesMulti.png";
+}
+ 
+function ToppingCookieMonster() {
+    document.getElementById("img-topping").src="/links/Topping_Cookiemonster.png";
+}
+
+function ToppingMarshmellows() {
+    document.getElementById("img-topping").src="/links/Topping_Marshmellows.png";
+}
+
+function ToppingChocoballs() {
+    document.getElementById("img-topping").src="/links/Topping_Chocoballs.png";
+}
+
+  
